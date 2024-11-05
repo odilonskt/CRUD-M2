0cd4e3d61213f31d9838cec9e8fa2f41ba0608a3
odilonskt <odilon123c@gmail.com>
2024-11-05 09:21:26 -0300

    adicionando link: no js e no index.html

--- js/hrefLocation.js
+++ js/hrefLocation.js
@@ -1,4 +1,4 @@
 document.getElementById('volta').addEventListener('click',(evento)=>{
     evento.preventDefault();
-    window.location.href ='index.html'
+    window.location.href ='https://odilonskt.github.io/CRUD-M2/'
 });
