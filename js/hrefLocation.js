4c38055487936b8e897fdacce2f2c8f31357108b
odilonskt <odilon123c@gmail.com>
2024-11-04 23:49:49 -0300

    refazendo o caminho

--- js/hrefLocation.js
+++ js/hrefLocation.js
@@ -1,4 +1,4 @@
 document.getElementById('volta').addEventListener('click',(evento)=>{
     evento.preventDefault();
-    window.location.href ='https://odilonskt.github.io/CRUD-M2/'
+    window.location.href ='index.html'
 });
